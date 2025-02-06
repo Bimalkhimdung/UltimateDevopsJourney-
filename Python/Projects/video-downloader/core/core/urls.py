813100bc21035344f071aef7c5b12a590b80fb09
bimal-rai <bimal.rai@realhrsoft.com>
2025-02-06 21:33:56 +0545

    up and running code

--- Python/Projects/video-downloader/core/core/urls.py
+++ Python/Projects/video-downloader/core/core/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from youtube_downloader.views import download_video, custom_404_view, custom_500_view
+
+handler404 = custom_404_view
+handler500 = custom_500_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", download_video, name='download_video')
+    #path("download/", __import__("youtube_downloader.views").views.download_video, name="download_video"),
+
 ]
